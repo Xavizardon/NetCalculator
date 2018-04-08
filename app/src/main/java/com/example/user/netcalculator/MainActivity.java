3b08cd108989c91c60f31eea49e0a04946c8b6f7
xavizardon <00013812@uca.edu.sv>
2018-04-07 23:57:07 -0600

    Resuelto: Se pusieron los "put" de Hashmap dentro de un metodo para no generar errores.

--- app/src/main/java/com/example/user/netcalculator/MainActivity.java
+++ app/src/main/java/com/example/user/netcalculator/MainActivity.java
@@ -8,53 +8,52 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.HashMap;
-import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
     EditText ipdir, mask;
     TextView shid, shbroad, shnet, shhost, nhost;
 
-    HashMap<Integer, String> equiv = new HashMap<Integer, String>();
-    equiv.put(32, "255.255.255.255");
-    equiv.put(31, "255.255.255.254");
-    equiv.put(30, "255.255.255.252");
-    equiv.put(29, "255.255.255.248");
-    equiv.put(28, "255.255.255.240");
-    equiv.put(27, "255.255.255.224");
-    equiv.put(26, "255.255.255.192");
-    equiv.put(25, "255.255.255.128");
-    equiv.put(24, "255.255.255.0");
-    equiv.put(23, "255.255.254.0");
-    equiv.put(22, "255.255.252.0");
-    equiv.put(21, "255.255.248.0");
-    equiv.put(20, "255.255.240.0");
-    equiv.put(19, "255.255.224.0");
-    equiv.put(18, "255.255.192.0");
-    equiv.put(17, "255.255.128.0");
-    equiv.put(16, "255.255.0.0");
-    equiv.put(15, "255.254.0.0");
-    equiv.put(14, "255.252.0.0");
-    equiv.put(13, "255.248.0.0");
-    equiv.put(12, "255.240.0.0");
-    equiv.put(11, "255.224.0.0");
-    equiv.put(10, "255.192.0.0");
-    equiv.put(9, "255.128.0.0");
-    equiv.put(8, "255.0.0.0");
-    equiv.put(7, "254.0.0.0");
-    equiv.put(6, "252.0.0.0");
-    equiv.put(5, "248.0.0.0");
-    equiv.put(4, "240.0.0.0");
-    equiv.put(3, "224.0.0.0");
-    equiv.put(2, "192.0.0.0");
-    equiv.put(1, "128.0.0.0");
-    equiv.put(0, "0.0.0.0");
-
+    public HashMap<Integer, String> equiv = new HashMap<Integer, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        equiv.put(32, "255.255.255.255");
+        equiv.put(31, "255.255.255.254");
+        equiv.put(30, "255.255.255.252");
+        equiv.put(29, "255.255.255.248");
+        equiv.put(28, "255.255.255.240");
+        equiv.put(27, "255.255.255.224");
+        equiv.put(26, "255.255.255.192");
+        equiv.put(25, "255.255.255.128");
+        equiv.put(24, "255.255.255.0");
+        equiv.put(23, "255.255.254.0");
+        equiv.put(22, "255.255.252.0");
+        equiv.put(21, "255.255.248.0");
+        equiv.put(20, "255.255.240.0");
+        equiv.put(19, "255.255.224.0");
+        equiv.put(18, "255.255.192.0");
+        equiv.put(17, "255.255.128.0");
+        equiv.put(16, "255.255.0.0");
+        equiv.put(15, "255.254.0.0");
+        equiv.put(14, "255.252.0.0");
+        equiv.put(13, "255.248.0.0");
+        equiv.put(12, "255.240.0.0");
+        equiv.put(11, "255.224.0.0");
+        equiv.put(10, "255.192.0.0");
+        equiv.put(9, "255.128.0.0");
+        equiv.put(8, "255.0.0.0");
+        equiv.put(7, "254.0.0.0");
+        equiv.put(6, "252.0.0.0");
+        equiv.put(5, "248.0.0.0");
+        equiv.put(4, "240.0.0.0");
+        equiv.put(3, "224.0.0.0");
+        equiv.put(2, "192.0.0.0");
+        equiv.put(1, "128.0.0.0");
+        equiv.put(0, "0.0.0.0");
 
         ipdir = (EditText) findViewById(R.id.ip);
         mask = (EditText) findViewById(R.id.mask);
